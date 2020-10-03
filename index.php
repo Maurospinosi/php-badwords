ed823d8eba570f69b2fb00b6731bd8de566bcaa7
Mauro <mauro.spinosi94@gmail.com>
2020-10-03 13:06:25 +0200

    END

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
-//questa è una variabile
-$name = "Michele";
+ $badword = $_GET["censura"];
+ $title = "Verso Divina Commedia";
+ $verso = ' "…VIDI UNO CON LA TESTA COSÌ SPORCA DI MERDA
+ CHE NON CAPIVO SE FOSSE UN LAICO O UN ECCLESIASTICO…" ';
+ $autore = "[cit.] Dante, canto XVIII";
+ $versoCens = str_replace("MERDA", $badword, $verso);
 ?>
-<h1>Ciao mi chiamo <?php echo $name ?></h1>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Php badwords</title>
+  </head>
+  <body>
+    <h1><?php echo $title; ?></h1>
+    <p><strong> <?php echo $versoCens; ?></strong></p>
+    <h3><?php echo $autore; ?></h3>
+  </body>
+</html>
